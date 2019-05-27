a0de159b0c7d0e10204485a3c7a9bc84b32b4129
bilalCh213 <boyz213@live.com>
2019-05-28 01:59:54 +0500

    Added Paper Art. Some work on Enemy Indicator [WIP]

---
+++ Assets/Scripts/GenerateEnemyIndicators.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//	WORK IN PROGRESS...
+
+public class GenerateEnemyIndicators : MonoBehaviour
+{
+	public GameManager indicatorPrefab;
+
+	[SerializeField] private Transform bottomLeft;
+	[SerializeField] private Transform topRight;
+
+    void Start()
+    {
+        
+    }
+	
+    void Update()
+    {
+        //Instantiate a Prefab [with Sprite (dim triangle) and IndicateTowards] setted to Enemy and those two transforms from here
+		//Have disable render when near enough to player
+    }
+}
