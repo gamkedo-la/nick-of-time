d77bfafb0367cb1f8ac418afef48bdb42ef7fc7e
Tyler Funk <tcfunk25@gmail.com>
2019-05-30 17:01:12 -0500

    basic animations set up for Door prefab

---
+++ Assets/Door.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private bool isOpen = false;
+
+    private Animator anim;
+    private Collider2D collider;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        collider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("Open", isOpen);
+        collider.enabled = !isOpen;
+    }
+}
