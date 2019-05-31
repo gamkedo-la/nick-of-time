5b5dead52cb9163ce67fa3cfa4d6f7ada135f357
Tyler Funk <tcfunk25@gmail.com>
2019-05-31 07:19:54 -0500

    use animator to change door collider, sprite sort order

---
+++ Assets/Scripts/Gameplay/Door.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private bool isOpen = false;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("Open", isOpen);
+    }
+}
