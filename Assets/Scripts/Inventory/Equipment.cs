1e7bd75e8bb8e77b69241eec08d9cecdd42681e0
brnielsen <47045697+brnielsen@users.noreply.github.com>
2019-06-01 09:08:00 -0400

    Added to inventory system (unfinished)

---
+++ Assets/Scripts/Inventory/Equipment.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Equipment", menuName = "Inventory/Equipment")]
+public class Equipment : Item
+{
+    public EquipmentSlot equipSlot;
+
+    public int damageModifier;
+
+    public override void Use()
+    {
+        base.Use();
+        EquipmentManager.instance.Equip(this);
+        RemoveFromInventory();
+    }
+}
+
+public enum EquipmentSlot { primary, secondary}
---
+++ Assets/Scripts/Inventory/EquipmentManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentManager : MonoBehaviour
+{
+    public static EquipmentManager instance;
+
+    Inventory inventory;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    Equipment[] currentEquipment;
+
+    private void Start()
+    {
+        int numberOfSlots = System.Enum.GetNames(typeof(EquipmentSlot)).Length;
+        currentEquipment = new Equipment[numberOfSlots];
+        inventory = Inventory.instance;
+    }
+
+    public void Equip(Equipment newItem) 
+    {
+        int slotIndex = (int)newItem.equipSlot;
+
+        Equipment oldItem = null;
+
+        if(currentEquipment[slotIndex] != null)
+        {
+            oldItem = currentEquipment[slotIndex];
+            inventory.Add(oldItem);
+        }
+
+        currentEquipment[slotIndex] = newItem;
+    }
+
+}
