7fc9af65df836cbaffd9be0a2b10ae8a62c3f9ba
bilalCh213 <boyz213@live.com>
2019-06-02 12:59:13 +0500

    Added Light Emitter Object Material

---
+++ Assets/LightEmitterObject.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightEmitterObject : MonoBehaviour
+{
+	public Color highLightColor;
+	public float highLightIntensity;
+	public float highLightRange;
+	public float highLightSpotAngle;
+
+	[Space]
+	public Color midLightColor;
+	public float midLightIntensity;
+	public float midLightRange;
+	public float midLightSpotAngle;
+
+	[Space]
+	public Color lowLightColor;
+	public Color holderLightColor;
+
+	private Material mat;
+
+	private Light denseLight;
+	private Light spreadLight;
+
+    void Start()
+    {
+		mat = gameObject.GetComponent<SpriteRenderer>().sharedMaterial;
+		denseLight = gameObject.transform.GetChild(0).GetChild(0).GetComponent<Light>();
+		spreadLight = gameObject.transform.GetChild(0).GetChild(1).GetComponent<Light>();
+	}
+
+	private void Update()
+	{
+		OnDrawGizmos();
+	}
+
+	void OnDrawGizmos()
+    {
+		mat.SetColor("highLightColor", highLightColor);
+		mat.SetColor("midLightColor", midLightColor);
+		mat.SetColor("lowLightColor", lowLightColor);
+		mat.SetColor("holderLightColor", holderLightColor);
+		
+		denseLight.color = highLightColor;
+		spreadLight.color = midLightColor;
+
+		denseLight.intensity = highLightIntensity;
+		spreadLight.intensity = midLightIntensity;
+
+		denseLight.range = highLightRange;
+		spreadLight.range = midLightRange;
+
+		denseLight.spotAngle = highLightSpotAngle;
+		spreadLight.spotAngle = midLightSpotAngle;
+	}
+}
