2035ea9240a2dda5342ec882ca6b480baa5e8314
bilalCh213 <boyz213@live.com>
2019-06-04 12:58:54 +0500

    Added some Image Effects

---
+++ Assets/Scripts/ImageEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ImageEffect : MonoBehaviour
+{
+	public int materialIndex = 0;
+	public Material[] imageEffectMaterial;
+
+	public int effectIteration = 0;
+
+	[Range(0f, 1f)]
+	public float value = 0;
+	
+	private void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		imageEffectMaterial[materialIndex].SetFloat("value", value);
+
+		RenderTexture rt = RenderTexture.GetTemporary(source.width, source.height);
+		Graphics.Blit(source, rt);
+
+		for (int i = 0; i < effectIteration; i++)
+		{
+			RenderTexture rt2 = RenderTexture.GetTemporary(source.width, source.height);
+			Graphics.Blit(rt, rt2, imageEffectMaterial[materialIndex]);
+			RenderTexture.ReleaseTemporary(rt);
+			rt = rt2;
+		}
+
+		Graphics.Blit(rt, destination);
+		RenderTexture.ReleaseTemporary(rt);
+	}
+}
