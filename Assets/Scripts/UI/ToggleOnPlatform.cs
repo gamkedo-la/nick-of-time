473e6a4a833837e599a827adfa0b35a42fe94674
bilalCh213 <boyz213@live.com>
2019-06-04 15:17:46 +0500

    On WebGL version, the Quit button will not display and Toggle button will be centered.

---
+++ Assets/Scripts/UI/ToggleOnPlatform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleOnPlatform : MonoBehaviour
+{
+	public GameObject[] objectsToEnable;
+	public GameObject[] objectsToDisable;
+
+    void Start()
+    {
+		//#if UNITY_EDITOR
+#if UNITY_WEBGL //#elif UNITY_WEBGL
+
+		foreach (var o in objectsToEnable)
+		{
+			o.SetActive(true);
+		}
+
+		foreach (var o in objectsToDisable)
+		{
+			o.SetActive(false);
+		}
+
+#endif
+	}
+}
