2850c440f99fd2a8806d99fbc84c85bd4b53463c
Tyler Funk <tcfunk25@gmail.com>
2019-06-10 17:12:12 -0500

    Portal transports player to exit portal

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public Portal[] exits;
+    bool active = true;
+
+    private void TransportPlayer(GameObject player, Portal exit)
+    {
+        exit.active = false;
+        player.transform.position = exit.transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (active && collision.CompareTag("Player"))
+        {
+            if (exits.Length == 1)
+            {
+                TransportPlayer(collision.gameObject, exits[0]);
+            }
+            else
+            {
+                // @TODO: Display portal options
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        active = true;
+    }
+}
