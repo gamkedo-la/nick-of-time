3d9fd99d087c3e5491e24aaf3ba973979a8e0ec2
bilalCh213 <boyz213@live.com>
2019-07-01 18:53:26 +0500

    Story2 Props Completed. Story3 Scene Tiling [WIP]. Added Portals, Key, Pressure plate and Doors to Story3. Removed Story2_Old. Changed the Pause Control => P, Tab and Delete keys causes pause. Removed Time speed cheat. Added Story3 Trigger Map.

---
+++ Assets/Scripts/InstantImpact.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class InstantImpact : MonoBehaviour
+{
+	public enum ImpactType
+	{
+		HP,
+		Stamina
+	};
+
+	[SerializeField] private ImpactType type = ImpactType.HP;
+	[SerializeField] private float value = 0.1f;
+	[SerializeField] private float minCollectDistance = 0.02f;
+	[SerializeField] private float magnetStrength = 0.01f;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.name.Contains("Player"))
+		{
+			if (Vector2.Distance(transform.position, collision.gameObject.transform.position) <= minCollectDistance)
+			{
+				if (type == ImpactType.HP)
+					collision.gameObject.GetComponent<HitCheck>().hp += value;
+				else if (type == ImpactType.Stamina)
+					collision.gameObject.GetComponent<PlayerController>().actionPoints += value;
+
+				Destroy(gameObject);
+			}
+			else
+			{
+				transform.position = Vector3.Lerp(transform.position, collision.gameObject.transform.position, magnetStrength);
+			}
+		}
+	}
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		if (collision.gameObject.name.Contains("Player"))
+		{
+			if (Vector2.Distance(transform.position, collision.gameObject.transform.position) <= minCollectDistance)
+			{
+				if (type == ImpactType.HP)
+					collision.gameObject.GetComponent<HitCheck>().hp += value;
+				else if (type == ImpactType.Stamina)
+					collision.gameObject.GetComponent<PlayerController>().actionPoints += value;
+
+				Destroy(gameObject);
+			}
+			else
+			{
+				transform.position = Vector3.Lerp(transform.position, collision.gameObject.transform.position, magnetStrength);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/NoParentalRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoParentalRotation : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+	
+    void Update()
+    {
+		transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+    }
+}
